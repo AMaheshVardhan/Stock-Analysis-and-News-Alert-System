302afe39b38cb9e44e62de9d2b553f1eea0b6735
Akena Mahesh Vardhan <113368513+Mahesh-Vardhan@users.noreply.github.com>
2023-08-19 18:53:57 +0530

    main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ NEWS_API_KEY = "71dd4f58ddf14e97865156e1459306e0"
 TWILIO_SID = "AC8d448bcfbf9d502bcfe8b2c42cb84404"
 TWILIO_AUTH_TOKEN = "292961033539b7d6400b50aa9052cd35"
 
+
 stock_params = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK_NAME,
